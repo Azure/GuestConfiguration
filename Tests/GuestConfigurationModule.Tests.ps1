acc16bba4874f6039bb6b353200c6067c9b84c3a
Michael Greene <migreene@JasonWork.redmond.corp.microsoft.com>
2019-11-13 10:53:29 -0600

    check for thumprint in test

--- Tests/GuestConfigurationModule.Tests.ps1
+++ Tests/GuestConfigurationModule.Tests.ps1
@@ -161,8 +161,9 @@ Import-Certificate -FilePath "$env:Temp/guestconfigurationtest/cert/exported.cer
         It 'Verify Protect-GuestConfigurationPackage cmdlet can sign policy package (Windows Only)' {
             if ($IsWindows) {
                 $Cert = Get-ChildItem -Path cert:/LocalMachine/My | Where-Object { ($_.Subject -eq "CN=testcert") } | Select-Object -First 1
-                if ($null -eq $Cert) {Write-Error 'no certificate was available for the test environment'}
-                
+                if ($null -eq $Cert) {write-warning 'no certificate was available for the test environment'}
+                if ($null -eq $Cert.thumbprint) {write-warning 'the certificate does not have a thumbprint'}
+
                 $package = New-GuestConfigurationPackage -Configuration $mofPath -Name $policyName -Path $outputFolder/package
                 
                 Protect-GuestConfigurationPackage -Path $package.Path -Certificate $Cert

9e678eb65c11488b4bcec8ce564ae3f988e4841f
Berhe Abrha <berha@microsoft.com>
2020-01-29 22:48:42 -0800

    Handle policy definition with no param

--- helpers/GuestConfigurationPolicy.psm1
+++ helpers/GuestConfigurationPolicy.psm1
@@ -1231,12 +1231,16 @@ function New-GuestConfigurationAuditPolicyDefinition {
     }
 
     $filePath = Join-Path -Path $FolderPath -ChildPath $FileName
-
+    $ParameterMapping  = @{}
+    $ParameterDefinitions  = @{}
     $auditPolicyContentHashtable = [Ordered]@{}
     if($PSCmdlet.ParameterSetName -eq 'AuditWithoutDINE')
     {
-        $ParameterMapping = Get-ParameterMappingForAINE $ParameterInfo
-        $ParameterDefinitions = Get-ParameterDefinitionsAINE $ParameterInfo
+        if ($null -ne $ParameterInfo)
+        {
+            $ParameterMapping = Get-ParameterMappingForAINE $ParameterInfo
+            $ParameterDefinitions = Get-ParameterDefinitionsAINE $ParameterInfo
+        }
         $auditPolicyContentHashtable = [Ordered]@{
             properties = [Ordered]@{
                 displayName = $DisplayName

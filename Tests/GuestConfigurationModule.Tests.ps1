39efbaadefb7de7d5862649e7104b02ce2771835
Michael Greene <michael.greene@microsoft.com>
2020-02-15 07:55:29 -0600

    resolve Publish test issues

--- Tests/GuestConfigurationModule.Tests.ps1
+++ Tests/GuestConfigurationModule.Tests.ps1
@@ -158,8 +158,8 @@ Import-Certificate -FilePath "$env:BuildFolder/guestconfigurationtest/cert/expor
                 Remove-Item "$Env:BuildTempFolder/Modules" -Force -Recurse
             }
         }
-        if (Test-Path "$env:BuildTempFolder") {
-            Remove-Item "$env:BuildTempFolder" -Recurse -Force
+        if (Test-Path "$Env:BuildTempFolder") {
+            Remove-Item "$Env:BuildTempFolder" -Recurse -Force
         }
     }
 

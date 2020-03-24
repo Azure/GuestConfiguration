f23caa48f903d2f63a132cf7d9b067c52d24dff7
Michael Greene <michael.greene@microsoft.com>
2020-03-24 10:02:32 -0500

    restore KeyEncipherment keyusage

--- Tests/GuestConfigurationModule.Tests.ps1
+++ Tests/GuestConfigurationModule.Tests.ps1
@@ -55,7 +55,7 @@ DSCConfig -OutputPath $dscConfigFolderPath
             New-SelfsignedCertificateEx `
                 -Subject "CN=testcert" `
                 -EKU 'Code Signing' `
-                -KeyUsage 'DataEncipherment, DigitalSignature' `
+                -KeyUsage 'KeyEncipherment, DataEncipherment, DigitalSignature' `
                 -FriendlyName 'DSC Credential Encryption certificate' `
                 -Exportable `
                 -StoreLocation 'LocalMachine' `

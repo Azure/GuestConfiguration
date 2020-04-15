48570c119c9a432d33187196e29ff50622bd5283
Katie Kragenbrink <kwirkykat@users.noreply.github.com>
2020-04-14 17:08:30 -0700

    1.20.0 - Switch InSpec download URLs to internal Azure Storage endpoints (#79)
    * [only affects tests] Reset tests to match BVT (#58)
    
    * Fix bug cannot index in to type psobject
    
    * update manifest
    
    * typo in changelog
    
    * modify new tests for CI environment
    
    * initialize machine
    
    * execution policy only on windows
    
    * Test- will not work on Mac until we mock path
    
    * test if this set of tests will work on Linux
    
    * bug in test condition
    
    * no mocks for policy so only test in proviate builds
    
    * No Linux; bug in test release env
    
    * pspki module
    
    * SAN dns
    
    * Test loading from local path; no psmodulepath change
    
    * edit psmodulepath
    
    * import from path
    
    * no need for proxy functions yet
    
    * initialize and then write info to see run environment
    
    * modify new tests for CI environment
    
    * initialize machine
    
    * no mocks for policy so only test in proviate builds
    
    * No Linux; bug in test release env
    
    * bug in Test-GCP; bring over from rollback  branch
    
    * Test loading from local path; no psmodulepath change
    
    * 1.19.3 bugfix for psobject index error
    
    * undo build script changes for cleaner PR
    
    * fix bugs from rebase
    
    * test cert issue
    
    * TEST protect using private pfx
    
    * only store cert as file
    
    * use securestring for private key password
    
    * use original script
    
    * copy cert to root store
    
    * Replace all Appveyor references with Azure DevOps
    
    * trim AzHelper
    
    * improvements based on code review
    
    * skip build if only changelog is udpated
    
    * Change download URLs in InSpec installation script
    
    * Call download with retries function to download InSpec
    
    * Fix concatenation of strings in InSpec installation script
    
    * Bash syntax fixes for InSpec installation script
    
    * Tiny style fixes to InSpec installation script
    
    * Increase max retry count for InSpec installation
    
    Co-authored-by: Michael Greene <mgreenegit@users.noreply.github.com>

---
+++ Tests/AzHelper.psm1
@@ -0,0 +1,100 @@
+$AZURE_AUTOMATION_TEST_SUBSCRIPTION = 'Guest Configuration Automation Test'
+$Azure_API_TOKEN_EXPIRATION_HR = 1
+$Azure_SUBSCRIPTION_ID = $Env:Azure_SUBSCRIPTION_ID
+if ((Test-Path Env:\DSC_AZUREDEVOPS_ENVIRONMENT_TEST_REGION) -and ($env:DSC_AZUREDEVOPS_ENVIRONMENT_TEST_REGION -eq 'Test'))
+{
+    # For adhoc testing in test region use a different subscription 
+    $Azure_SUBSCRIPTION_ID = $Env:Azure_Test_SUBSCRIPTION_ID
+}
+
+<## Install ##>
+
+function Install-AzLibraries{
+    param()
+    
+    #Install-packageprovider nuget -force
+    
+    # Install Az Libraries
+    Install-Module Az -AllowClobber -Force | Out-Null
+    
+    Import-Module Az -Force 
+    
+}
+
+<#
+.Synopsis
+   Test if current enviroment is Azure DevOps worker enviroment.
+.DESCRIPTION
+   If the current enviroment is Azure DevOps worker enviroment the varaibles defined in the projects Azure DevOps.yml will be availabe.
+.EXAMPLE
+   Test-ServicePrincipalAccountInEnviroment 
+#>
+function Test-ServicePrincipalAccountInEnviroment
+{
+    param()
+     return (Test-Path Env:/AzureServicePrincipalPassword) -and`
+            (Test-Path Env:/AzureServicePrincipalUserName) -and`
+            (Test-Path Env:/AzureTenantID)
+}
+
+<#
+.Synopsis
+   Login to Azure Service
+.DESCRIPTION
+   This will log-in to azure using a service principal account information is available on the current enviroment.
+.EXAMPLE
+   Login-ToTestAzureAcccountpackageprovider 
+#>
+function Login-ToTestAzAccount
+{
+      param(
+          [string]
+          $SubscriptionID=$Azure_SUBSCRIPTION_ID
+      )
+       
+      if (-not(Test-ServicePrincipalAccountInEnviroment))
+      {
+          throw "Test Service Principal account is only availabe under the Azure DevOps worker context"
+      }
+
+    
+      $secPassword = ConvertTo-SecureString $env:AzureServicePrincipalPassword -AsPlainText -Force
+      $servicePrincipalCreds = [System.Management.Automation.PSCredential]::new($env:AzureServicePrincipalUserName, $secPassword)
+      Login-AzAccount -Credential $servicePrincipalCreds -ServicePrincipal -TenantId $env:AzureTenantID
+      Select-AzSubscription -SubscriptionId $SubscriptionID
+}
+
+<#
+.Synopsis
+   Generate Azure Service token. Detail description of access token can be found here https://msdn.microsoft.com/en-us/library/hh454950.aspx
+.DESCRIPTION
+   This token will be generated using the test service principal account and will have 1 hr duration for expriation and will be used to invoke Azure REST APIs
+.EXAMPLE
+   Get-AzureAccessTokenInAzureDevOps
+#>
+function Get-AzureAccessTokenInAzureDevOps
+{
+    param()
+    if (-not(Test-ServicePrincipalAccountInEnviroment))
+    {
+      throw "Test Service Principal account is only availabe under the Azure DevOps worker context"
+    }
+  
+    $TokenEndpoint = {https://login.windows.net/{0}/oauth2/token} -f $env:AzureTenantID
+    $ARMResource = "https://management.core.windows.net/";
+    $Body = @{
+            'resource'= $ARMResource
+            'client_id' = $env:AzureServicePrincipalUserName
+            'grant_type' = 'client_credentials'
+            'client_secret' =  $env:AzureServicePrincipalPassword
+    }    
+    $params = @{
+        ContentType = 'application/x-www-form-urlencoded'
+        Headers = @{'accept'='application/json'}
+        Body = $Body
+        Method = 'Post'
+        URI = $TokenEndpoint
+    }    
+    $token = Invoke-RestMethod @params
+    return $token
+}

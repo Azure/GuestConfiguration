9fb1db2af82b21cb22db7cbd22434e0ad011924b
Michael Greene <michael.greene@microsoft.com>
2020-05-14 12:30:27 -0500

    only handle error and postpone native resource issue

--- helpers/GuestConfigurationPolicy.psm1
+++ helpers/GuestConfigurationPolicy.psm1
@@ -89,7 +89,7 @@ function Test-GuestConfigurationMofResourceDependencies {
     }
 }
 
-function Copy-DSCResources {
+function Copy-DscResources {
     [CmdletBinding()]
     param
     (
@@ -103,56 +103,38 @@ function Copy-DSCResources {
     )
     $resourcesInMofDocument = [Microsoft.PowerShell.DesiredStateConfiguration.Internal.DscClassCache]::ImportInstances($MofDocumentPath, 4)
 
-    # Copies DSC resource modules
     Write-Verbose "Copy DSC resources ..."
-    $modulesToCopy = @{ }
     $modulePath = New-Item -ItemType Directory -Force -Path (Join-Path $Destination 'Modules')
+    $guestConfigModulePath = New-Item -ItemType Directory -Force -Path (Join-Path $modulePath 'GuestConfiguration')
+    try {
+        $latestModule = @()
+        $latestModule += Get-Module GuestConfiguration
+        $latestModule += Get-Module GuestConfiguration -ListAvailable
+        $latestModule = ($latestModule | Sort-Object Version)[0]
+    }
+    catch {
+        write-error 'unable to find the GuestConfiguration module either as an imported module or in $env:PSModulePath'
+    }
+    Copy-Item "$($latestModule.ModuleBase)/DscResources/" "$guestConfigModulePath/DscResources/" -Recurse
+    Copy-Item "$($latestModule.ModuleBase)/helpers/" "$guestConfigModulePath/helpers/" -Recurse
+    Copy-Item "$($latestModule.ModuleBase)/GuestConfiguration.psd1" "$guestConfigModulePath/GuestConfiguration.psd1"
+    Copy-Item "$($latestModule.ModuleBase)/GuestConfiguration.psm1" "$guestConfigModulePath/GuestConfiguration.psm1"
+    
+    # Copies DSC resource modules
+    $modulesToCopy = @{ }
     $resourcesInMofDocument | ForEach-Object {
         if ($_.CimInstanceProperties.Name -contains 'ModuleName' -and $_.CimInstanceProperties.Name -contains 'ModuleVersion') {
-            if ($_.ModuleName -eq 'GuestConfiguration') {
-                Write-Verbose "Copy Guest Configuration native resources ..."
-                $guestConfigModulePath = New-Item -ItemType Directory -Force -Path (Join-Path $modulePath 'GuestConfiguration')
-                try {
-                    # We need to check both the versions imported in to the current session and the versions available in psmodulepath
-                    $latestModule = @()
-                    $latestModule += Get-Module GuestConfiguration
-                    $latestModule += Get-Module GuestConfiguration -ListAvailable
-                    $latestModule = ($latestModule | Sort-Object Version)[0]
-                }
-                catch {
-                    write-error 'Unable to find the GuestConfiguration module either as an imported module or in $env:PSModulePath'
-                }
-                Copy-Item "$($latestModule.ModuleBase)/DSCResources/" "$guestConfigModulePath/DSCResources/" -Recurse
-                Copy-Item "$($latestModule.ModuleBase)/helpers/" "$guestConfigModulePath/helpers/" -Recurse
-                Copy-Item "$($latestModule.ModuleBase)/GuestConfiguration.psd1" "$guestConfigModulePath/GuestConfiguration.psd1"
-                Copy-Item "$($latestModule.ModuleBase)/GuestConfiguration.psm1" "$guestConfigModulePath/GuestConfiguration.psm1"
-    
-                # Copy native resources.
-                $nativeResourcePath = New-Item -ItemType Directory -Force -Path (Join-Path $modulePath 'DscNativeResources')
-                $resources = Get-DscResource -Module GuestConfiguration
-                $resources | ForEach-Object {
-                    if ($_.ImplementedAs -eq 'Binary') {
-                        $binaryResourcePath = Join-Path (Join-Path $latestModule.ModuleBase 'DSCResources') $_.ResourceType
-                        Get-ChildItem $binaryResourcePath/* -Include *.sh | ForEach-Object { Convert-FileToUnixLineEndings -FilePath $_ }
-                        Copy-Item $binaryResourcePath $nativeResourcePath -Recurse -Force
-                    }
-                }
-            }
-            if ($_.ModuleName -eq 'PSDesiredStateConfiguration') {
-                Write-Error 'The configuration includes DSC resources from the Windows PowerShell 5.1 module "PSDesiredStateConfiguration" that are not available in PowerShell Core. Switch to the "PSDSCResources" module available from the PowerShell Gallery. Note that the File and Package resources are not yet available in "PSDSCResources".'
-            }
-            if ($_.ModuleName -ne 'GuestConfiguration' -AND $_.ModuleName -ne 'PSDesiredStateConfiguration') {
+            if ($_.ModuleName -ne 'GuestConfiguration') {
                 $modulesToCopy[$_.CimClass.CimClassName] = @{ModuleName = $_.ModuleName; ModuleVersion = $_.ModuleVersion }
             }
         }
     }
 
-    # Add PowerShell modules required by DSC resource module
+    # PowerShell modules required by DSC resource module
     $powershellModulesToCopy = @{ }
     $modulesToCopy.Values | ForEach-Object {
         if ($_.ModuleName -ne 'GuestConfiguration') {
             $requiredModule = Get-Module -FullyQualifiedName @{ModuleName = $_.ModuleName; RequiredVersion = $_.ModuleVersion } -ListAvailable
-            if (!$requiredModule) {throw "Module '$($_.ModuleName)' with version '$($_.ModuleVersion)' was not found in Env:PSModulePath"}
             if (($requiredModule | Get-Member -MemberType 'Property' | ForEach-Object { $_.Name }) -contains 'RequiredModules') {
                 $requiredModule.RequiredModules | ForEach-Object {
                     if ($null -ne $_.Version) {
@@ -166,12 +148,15 @@ function Copy-DSCResources {
             }
         }
     }
+
     $modulesToCopy += $powershellModulesToCopy
 
-    # Copy modules
     $modulesToCopy.Values | ForEach-Object {
         $moduleToCopy = Get-Module -FullyQualifiedName @{ModuleName = $_.ModuleName; RequiredVersion = $_.ModuleVersion } -ListAvailable
         if ($null -ne $moduleToCopy) {
+            if ($_.ModuleName -eq 'PSDesiredStateConfiguration') {
+                Write-Error 'The configuration includes DSC resources from the Windows PowerShell 5.1 module "PSDesiredStateConfiguration" that are not available in PowerShell Core. Switch to the "PSDSCResources" module available from the PowerShell Gallery. Note that the File and Package resources are not yet available in "PSDSCResources".'
+            }
             $moduleToCopyPath = New-Item -ItemType Directory -Force -Path (Join-Path $modulePath $_.ModuleName)
             Copy-Item "$($moduleToCopy.ModuleBase)/*" $moduleToCopyPath -Recurse -Force
         }
@@ -180,8 +165,18 @@ function Copy-DSCResources {
         }
     }
 
+    # Copy binary resources.
+    $nativeResourcePath = New-Item -ItemType Directory -Force -Path (Join-Path $modulePath 'DscNativeResources')
+    $resources = Get-DscResource -Module GuestConfiguration
+    $resources | ForEach-Object {
+        if ($_.ImplementedAs -eq 'Binary') {
+            $binaryResourcePath = Join-Path (Join-Path $latestModule.ModuleBase 'DscResources') $_.ResourceType
+            Get-ChildItem $binaryResourcePath/* -Include *.sh | ForEach-Object { Convert-FileToUnixLineEndings -FilePath $_ }
+            Copy-Item $binaryResourcePath $nativeResourcePath -Recurse -Force
+        }
+    }
+
     # Remove DSC binaries from package (just a safeguard).
-    $guestConfigModulePath = Join-Path $modulePath 'GuestConfiguration'
     $binaryPath = Join-Path $guestConfigModulePath 'bin'
     Remove-Item -Path $binaryPath -Force -Recurse -ErrorAction 'SilentlyContinue' | Out-Null
 }

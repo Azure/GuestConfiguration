c5aa38b1577b1128834a426428e740274d065406
Michael Greene <michael.greene@microsoft.com>
2020-11-11 16:44:28 -0600

    restore get-gcmofcontent function

--- helpers/GuestConfigurationPolicy.psm1
+++ helpers/GuestConfigurationPolicy.psm1
@@ -336,6 +336,21 @@ function Get-GuestConfigurationMofContent {
     Write-Verbose "Parsing Configuration document '$Path'"
     $resourcesInMofDocument = [Microsoft.PowerShell.DesiredStateConfiguration.Internal.DscClassCache]::ImportInstances($Path, 4)
 
+    # Set the profile path for Chef resource
+    $resourcesInMofDocument | ForEach-Object {
+        if ($_.CimClass.CimClassName -eq 'MSFT_ChefInSpecResource') {
+            $profilePath = "$Name/Modules/$($_.Name)"
+            $item = $_.CimInstanceProperties.Item('GithubPath')
+            if ($item -eq $null) {
+                $item = [Microsoft.Management.Infrastructure.CimProperty]::Create('GithubPath', $profilePath, [Microsoft.Management.Infrastructure.CimFlags]::Property)                      
+                $_.CimInstanceProperties.Add($item) 
+            }
+            else {
+                $item.Value = $profilePath
+            }
+        }
+    }
+
     return $resourcesInMofDocument
 }
 

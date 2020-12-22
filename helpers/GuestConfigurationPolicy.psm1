3d77d8290050d7efaf5756cbbe16852cc3aeeec1
Michael Greene <michael.greene@microsoft.com>
2020-12-21 21:00:53 -0600

    use latest version

--- helpers/GuestConfigurationPolicy.psm1
+++ helpers/GuestConfigurationPolicy.psm1
@@ -2138,7 +2138,7 @@ function New-PesterResourceSection {
         $index = 1
     )
 
-    $Version = (Get-Module 'GuestConfiguration').Version
+    $Version = Get-Module 'GuestConfiguration' | Sort-Object 'Version' | Select-Object -Last 1 | ForEach-Object {$_.Version}
     
     # this is a workaround for inserting the variable in the middle of a word inside a here-string
     $ref = '$MSFT_PesterResource'+$Index+'ref'

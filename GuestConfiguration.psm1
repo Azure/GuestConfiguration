69c9f1117d6f8206d9450f2d9780af02d34668a0
Michael Greene <mgreenegit@users.noreply.github.com>
2020-12-23 11:17:33 -0600

    Feature/pipeline to new package (#146)
    * pipeline in for new-package
    
    * meta update
    
    * also add to 'Name' param

--- GuestConfiguration.psm1
+++ GuestConfiguration.psm1
@@ -43,11 +43,11 @@ InitReleaseVersionInfo $GuestConfigurationManifest.moduleVersion
 function New-GuestConfigurationPackage {
     [CmdletBinding()]
     param (
-        [parameter(Position = 0, Mandatory = $true)]
+        [parameter(Position = 0, Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
         [ValidateNotNullOrEmpty()]
         [string] $Name,
 
-        [parameter(Position = 1, Mandatory = $true)]
+        [parameter(Position = 1, Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
         [ValidateNotNullOrEmpty()]
         [string] $Configuration,
 

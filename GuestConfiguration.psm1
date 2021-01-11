42e0c4dd49bc85763bec2c8b07b12b397a9da529
Michael Greene <michael.greene@microsoft.com>
2021-01-11 15:32:53 -0600

    restore publish

--- GuestConfiguration.psm1
+++ GuestConfiguration.psm1
@@ -693,30 +693,33 @@ function Publish-GuestConfigurationPolicy {
 
     # Publish policies
     $subscriptionId = $rmContext.Subscription.Id
-    $policyFile = join-path $Path "AuditIfNotExists.json"
-    $jsonDefinition = Get-Content $policyFile | ConvertFrom-Json | ForEach-Object { $_ }
-    $definitionContent = $jsonDefinition.Properties
+    foreach ($policy in @("AuditIfNotExists.json", "DeployIfNotExists.json")){
+        $policyFile = join-path $Path $policy
+        $jsonDefinition = Get-Content $policyFile | ConvertFrom-Json | ForEach-Object {$_}
+        $definitionContent = $jsonDefinition.Properties
 
-    $newAzureRmPolicyDefinitionParameters = @{
-        Name        = $jsonDefinition.name
-        DisplayName = $($definitionContent.DisplayName | ConvertTo-Json -Depth 20).replace('"', '')
-        Description = $($definitionContent.Description | ConvertTo-Json -Depth 20).replace('"', '')
-        Policy      = $($definitionContent.policyRule | ConvertTo-Json -Depth 20)
-        Metadata    = $($definitionContent.Metadata | ConvertTo-Json -Depth 20)
-        ApiVersion  = '2018-05-01'
-        Verbose     = $true
+        $newAzureRmPolicyDefinitionParameters = @{
+            Name = $jsonDefinition.name
+            DisplayName = $($definitionContent.DisplayName | ConvertTo-Json -Depth 20).replace('"','')
+            Description = $($definitionContent.Description | ConvertTo-Json -Depth 20).replace('"','')
+            Policy = $($definitionContent.policyRule | ConvertTo-Json -Depth 20)
+            Metadata = $($definitionContent.Metadata | ConvertTo-Json -Depth 20)
+            ApiVersion = '2018-05-01'
+            Verbose = $true
+        }
+
+        if ($definitionContent.PSObject.Properties.Name -contains 'parameters')
+        {
+            $newAzureRmPolicyDefinitionParameters['Parameter'] = ConvertTo-Json -InputObject $definitionContent.parameters -Depth 15
+        }
+
+        if ($ManagementGroupName) {
+            $newAzureRmPolicyDefinitionParameters['ManagementGroupName'] = $ManagementGroupName
+        }
+
+        Write-Verbose "Publishing '$($jsonDefinition.properties.displayName)' ..."
+        New-AzPolicyDefinition @newAzureRmPolicyDefinitionParameters
     }
-
-    if ($definitionContent.PSObject.Properties.Name -contains 'parameters') {
-        $newAzureRmPolicyDefinitionParameters['Parameter'] = ConvertTo-Json -InputObject $definitionContent.parameters -Depth 15
-    }
-
-    if ($ManagementGroupName) {
-        $newAzureRmPolicyDefinitionParameters['ManagementGroupName'] = $ManagementGroupName
-    }
-
-    Write-Verbose "Publishing '$($jsonDefinition.properties.displayName)' ..."
-    New-AzPolicyDefinition @newAzureRmPolicyDefinitionParameters
 }
 
 Export-ModuleMember -Function @('New-GuestConfigurationPackage', 'Test-GuestConfigurationPackage', 'Protect-GuestConfigurationPackage', 'Publish-GuestConfigurationPackage', 'New-GuestConfigurationPolicy', 'Publish-GuestConfigurationPolicy')

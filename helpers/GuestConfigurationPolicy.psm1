74b43888665542ebb6e77dab9ed13418a8c12543
gaelcolas <gaelcolas@synedgy.com>
2021-02-02 08:48:53 +0100

    fix #153 duplication of helpers folders

--- helpers/GuestConfigurationPolicy.psm1
+++ helpers/GuestConfigurationPolicy.psm1
@@ -119,7 +119,6 @@ function Copy-DscResources {
         write-error 'unable to find the GuestConfiguration module either as an imported module or in $env:PSModulePath'
     }
     Copy-Item "$($latestModule.ModuleBase)/DscResources/" "$guestConfigModulePath/DscResources/" -Recurse -Force
-    Copy-Item "$($latestModule.ModuleBase)/helpers/" "$guestConfigModulePath/helpers/" -Recurse -Force
     Copy-Item "$($latestModule.ModuleBase)/GuestConfiguration.psd1" "$guestConfigModulePath/GuestConfiguration.psd1" -Force
     Copy-Item "$($latestModule.ModuleBase)/GuestConfiguration.psm1" "$guestConfigModulePath/GuestConfiguration.psm1" -Force
     

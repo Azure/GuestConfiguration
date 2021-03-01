d0b5274d8d3d0d20975149aa5a625d127dd3d8b0
gaelcolas <gaelcolas@synedgy.com>
2021-03-01 14:27:13 +0100

    moving DscOperations helper module to single function per file

---
+++ source/Modules/DscOperations/Public/Set-DscLocalConfigurationManager.ps1
@@ -0,0 +1,36 @@
+<#
+    .SYNOPSIS
+        Set DSC LCM settings.
+
+    .Parameter ConfigurationName
+        Configuration name.
+
+    .Example
+        Set-DscLocalConfigurationManager -Path C:\metaconfig
+#>
+
+function Set-DscLocalConfigurationManager
+{
+    [CmdletBinding()]
+    param (
+        [parameter(Position=0, Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $ConfigurationName,
+
+        [parameter(Position=1, Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Path
+    )
+
+    $job_id = [guid]::NewGuid().Guid
+    $gcBinPath = Get-GuestConfigBinaryPath
+    $dsclibPath = $(Get-DscLibPath) -replace  '[""\\]','\$&'
+
+    if(-not ([System.Management.Automation.PSTypeName]'GuestConfig.DscOperations').Type) {
+        $addTypeScript = $ExecuteDscOperationsScript -f $dsclibPath
+        Add-Type -TypeDefinition $addTypeScript -ReferencedAssemblies 'System.Management.Automation','System.Console','System.Collections'
+    }
+
+    $dscOperation = [GuestConfig.DscOperations]::New()
+    $result = $dscOperation.SetDscLocalConfigurationManager($PSCmdlet, $job_id, $ConfigurationName, $gcBinPath, $Path)
+}
---
+++ source/Modules/DscOperations/Public/Test-DscConfiguration.ps1
@@ -0,0 +1,34 @@
+<#
+    .SYNOPSIS
+        Test DSC configuration.
+
+    .Parameter ConfigurationName
+        Configuration name.
+
+    .Example
+        Test-DscConfiguration -ConfigurationName WindowsTLS
+#>
+
+function Test-DscConfiguration
+{
+    [CmdletBinding()]
+    param (
+        [parameter(Position=0, Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $ConfigurationName
+    )
+
+    $job_id = [guid]::NewGuid().Guid
+    $gcBinPath = Get-GuestConfigBinaryPath
+    $dsclibPath = $(Get-DscLibPath) -replace  '[""\\]','\$&'
+
+    if(-not ([System.Management.Automation.PSTypeName]'GuestConfig.DscOperations').Type) {
+        $addTypeScript = $ExecuteDscOperationsScript -f $dsclibPath
+        Add-Type -TypeDefinition $addTypeScript -ReferencedAssemblies 'System.Management.Automation','System.Console','System.Collections'
+    }
+
+    $dscOperation = [GuestConfig.DscOperations]::New()
+    $result = $dscOperation.TestDscConfiguration($PSCmdlet, $job_id, $ConfigurationName, $gcBinPath)
+
+    return ConvertFrom-Json $result
+}

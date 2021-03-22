adaa2263f3c3c1a7ddbe401e1d2f306d39f85df3
Michelle Yang <micy@microsoft.com>
2021-03-22 15:00:23 -0700

    Add Install-GuestConfigurationPackage function

--- GuestConfiguration.psm1
+++ GuestConfiguration.psm1
@@ -70,7 +70,7 @@ function New-GuestConfigurationPackage {
         if (-not (Test-Path -Path $Configuration -PathType Leaf)) {
             Throw "Invalid mof file path, please specify full file path for dsc configuration in -Configuration parameter."
         }
-         
+
         Write-Verbose "Creating Guest Configuration package in temporary directory '$unzippedPackagePath'"
 
         # Verify that only supported resources are used in DSC configuration.
@@ -99,7 +99,7 @@ function New-GuestConfigurationPackage {
                 Copy-Item -Path $FilesToInclude -Destination $modulePath -Recurse  -Force:$Force
             }
         }
-        
+
         # Create Guest Configuration Package.
         $packagePath = Join-Path $Path $Name
         New-Item -ItemType Directory -Force -Path $packagePath | Out-Null
@@ -120,6 +120,115 @@ function New-GuestConfigurationPackage {
     Finally {
     }
 }
+
+<#
+    .SYNOPSIS
+        Installs a Guest Configuration policy package.
+
+    .Parameter Path
+        Full path of the zipped Guest Configuration package.
+
+    .Parameter Parameter
+        Policy parameters.
+
+    .Example
+        Install-GuestConfigurationPackage -Path ./custom_policy/WindowsTLS.zip
+
+        $Parameter = @(
+            @{
+                ResourceType = "Service"            # dsc configuration resource type (mandatory)
+                ResourceId = 'windowsService'       # dsc configuration resource property id (mandatory)
+                ResourcePropertyName = "Name"       # dsc configuration resource property name (mandatory)
+                ResourcePropertyValue = 'winrm'     # dsc configuration resource property value (mandatory)
+            })
+
+        Install-GuestConfigurationPackage -Path ./custom_policy/AuditWindowsService.zip -Parameter $Parameter
+
+    .OUTPUTS
+        Returns compliance details.
+#>
+
+function Install-GuestConfigurationPackage {
+    [CmdletBinding()]
+    param (
+        [parameter(Position = 0, Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Path,
+
+        [parameter(Mandatory = $false)]
+        [Hashtable[]] $Parameter = @()
+    )
+    if ($env:OS -notmatch "Windows" -and $IsMacOS) {
+        Throw 'The Test-GuestConfigurationPackage cmdlet is not supported on MacOS'
+    }
+
+    if (-not (Test-Path $Path -PathType Leaf)) {
+        Throw 'Invalid Guest Configuration package path : $($Path)'
+    }
+
+    $verbose = ($PSBoundParameters.ContainsKey("Verbose") -and ($PSBoundParameters["Verbose"] -eq $true))
+    $systemPSModulePath = [Environment]::GetEnvironmentVariable("PSModulePath", "Process")
+
+    Try {
+        # Create policy folder
+        $Path = Resolve-Path $Path
+        $policyPath = Join-Path $(Get-GuestConfigPolicyPath) ([System.IO.Path]::GetFileNameWithoutExtension($Path))
+        Remove-Item $policyPath -Recurse -Force -ErrorAction SilentlyContinue
+        New-Item -ItemType Directory -Force -Path $policyPath | Out-Null
+
+        # Unzip policy package.
+        Expand-Archive -LiteralPath $Path $policyPath
+
+        # Get policy name
+        $dscDocument = Get-ChildItem -Path $policyPath -Filter *.mof
+        if (-not $dscDocument) {
+            Throw "Invalid policy package, failed to find dsc document in policy package."
+        }
+        $policyName = [System.IO.Path]::GetFileNameWithoutExtension($dscDocument)
+
+        # update configuration parameters
+        if ($Parameter.Count -gt 0) {
+            Update-MofDocumentParameters -Path $dscDocument.FullName -Parameter $Parameter
+        }
+
+        # Unzip Guest Configuration binaries
+        $gcBinPath = Get-GuestConfigBinaryPath
+        $gcBinRootPath = Get-GuestConfigBinaryRootPath
+        if (-not (Test-Path $gcBinPath)) {
+            # Clean the bin folder
+            Remove-Item $gcBinRootPath'\*' -Recurse -Force -ErrorAction SilentlyContinue
+
+            $zippedBinaryPath = Join-Path $(Get-GuestConfigurationModulePath) 'bin'
+            if ($(Get-OSPlatform) -eq 'Windows') {
+                $zippedBinaryPath = Join-Path $zippedBinaryPath 'DSC_Windows.zip'
+            }
+            else {
+                # Linux zip package contains an additional DSC folder
+                # Remove DSC folder from binary path to avoid two nested DSC folders.
+                New-Item -ItemType Directory -Force -Path $gcBinPath | Out-Null
+                $gcBinPath = (Get-Item $gcBinPath).Parent.FullName
+                $zippedBinaryPath = Join-Path $zippedBinaryPath 'DSC_Linux.zip'
+            }
+            [System.IO.Compression.ZipFile]::ExtractToDirectory($zippedBinaryPath, $gcBinPath)
+        }
+
+        # Publish policy package
+        Publish-DscConfiguration -ConfigurationName $policyName -Path $policyPath -Verbose:$verbose
+
+        # Set LCM settings to force load powershell module.
+        $metaConfigPath = Join-Path $policyPath "$policyName.metaconfig.json"
+        "{""debugMode"":""ForceModuleImport""}" | Out-File $metaConfigPath -Encoding ascii
+        Set-DscLocalConfigurationManager -ConfigurationName $policyName -Path $policyPath -Verbose:$verbose
+
+        # Clear Inspec profiles
+        Remove-Item $(Get-InspecProfilePath) -Recurse -Force -ErrorAction SilentlyContinue
+    }
+    Finally {
+        $env:PSModulePath = $systemPSModulePath
+    }
+
+}
+
 
 <#
     .SYNOPSIS
@@ -162,11 +271,11 @@ function Test-GuestConfigurationPackage {
         [Hashtable[]] $Parameter = @()
     )
 
-   
+
     if ($env:OS -notmatch "Windows" -and $IsMacOS) {
         Throw 'The Test-GuestConfigurationPackage cmdlet is not supported on MacOS'
     }
-    
+
     if (-not (Test-Path $Path -PathType Leaf)) {
         Throw 'Invalid Guest Configuration package path : $($Path)'
     }
@@ -230,7 +339,7 @@ function Test-GuestConfigurationPackage {
 
         # For Set scenario, run Start-Dsc command
         if ($Type -eq "Set") {
-            Start-DscConfiguration -ConfigurationName $policyName -Verbose:$verbose 
+            Start-DscConfiguration -ConfigurationName $policyName -Verbose:$verbose
         }
 
         $testResult = Test-DscConfiguration -ConfigurationName $policyName -Verbose:$verbose
@@ -416,7 +525,7 @@ function Protect-GuestConfigurationPackage {
         of a malicious person discovering the published content.
 
         Requires a resource group, storage account, and container
-        to be pre-staged. For details on how to pre-stage these things see the 
+        to be pre-staged. For details on how to pre-stage these things see the
         documentation for the Az Storage cmdlets.
         https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-powershell.
 
@@ -456,7 +565,7 @@ function Publish-GuestConfigurationPackage {
         [string] $StorageAccountName,
 
         [string] $StorageContainerName = 'guestconfiguration',
-        
+
         [switch] $Force
     )
 
@@ -537,7 +646,7 @@ function Publish-GuestConfigurationPackage {
                                  -ContentUri https://github.com/azure/auditservice/release/AuditService.zip `
                                  -DisplayName 'Monitor Windows Service Policy.' `
                                  -Description 'Policy to monitor service on Windows machine.' `
-                                 -Version 1.0.0.0 
+                                 -Version 1.0.0.0
                                  -Path ./git/custom_policy
                                  -Tag @{Owner = 'WebTeam'}
 
@@ -556,9 +665,9 @@ function Publish-GuestConfigurationPackage {
             New-GuestConfigurationPolicy -ContentUri 'https://github.com/azure/auditservice/release/AuditService.zip' `
                                  -DisplayName 'Monitor Windows Service Policy.' `
                                  -Description 'Policy to monitor service on Windows machine.' `
-                                 -Version 1.0.0.0 
+                                 -Version 1.0.0.0
                                  -Path ./policyDefinitions `
-                                 -Parameter $PolicyParameterInfo 
+                                 -Parameter $PolicyParameterInfo
 
     .OUTPUTS
         Return name and path of the Guest Configuration policy definitions.
@@ -656,7 +765,7 @@ function New-GuestConfigurationPolicy {
             Tag                      = $Tag
         }
         New-CustomGuestConfigPolicy -PolicyFolderPath $policyDefinitionsPath -AuditIfNotExistsInfo $AuditIfNotExistsInfo -Verbose:$verbose | Out-Null
-            
+
         $result = [pscustomobject]@{
             Name = $policyName
             Path = $Path
@@ -723,4 +832,4 @@ function Publish-GuestConfigurationPolicy {
     New-AzPolicyDefinition @newAzureRmPolicyDefinitionParameters
 }
 
-Export-ModuleMember -Function @('New-GuestConfigurationPackage', 'Test-GuestConfigurationPackage', 'Protect-GuestConfigurationPackage', 'Publish-GuestConfigurationPackage', 'New-GuestConfigurationPolicy', 'Publish-GuestConfigurationPolicy')
+Export-ModuleMember -Function @('New-GuestConfigurationPackage', 'Test-GuestConfigurationPackage', 'Install-GuestConfigurationPackage', 'Protect-GuestConfigurationPackage', 'Publish-GuestConfigurationPackage', 'New-GuestConfigurationPolicy', 'Publish-GuestConfigurationPolicy')

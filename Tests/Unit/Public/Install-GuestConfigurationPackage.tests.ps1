1f91512dc8311fdfe836feb93a930a6eb39eb9fd
gaelcolas <gaelcolas@synedgy.com>
2021-05-11 13:36:38 +0200

    fixing pester 5.2 issue & enabling PS7DscSupport exp feature in azdo

--- Tests/Unit/Public/Install-GuestConfigurationPackage.tests.ps1
+++ Tests/Unit/Public/Install-GuestConfigurationPackage.tests.ps1
@@ -1,15 +1,16 @@
 BeforeDiscovery {
-    $script:projectPath = "$PSScriptRoot/../../.." | Convert-Path
-    $script:projectName = Get-SamplerProjectName -BuildRoot $script:projectPath
+    $projectPath = "$PSScriptRoot/../../.." | Convert-Path
+    $projectName = Get-SamplerProjectName -BuildRoot $projectPath
 
-    Get-Module $script:projectName | Remove-Module -Force -ErrorAction SilentlyContinue
-    $script:importedModule = Import-Module $script:projectName -Force -PassThru -ErrorAction 'Stop'
-
-    $IsNotAzureDevOps = [string]::IsNullOrEmpty($env:ADO)
-    $IsNotWindowsAndIsAzureDevOps = ($IsLinux -or $IsMacOS) -AND $env:ADO
+    Get-Module $projectName | Remove-Module -Force -ErrorAction SilentlyContinue
+    $importedModule = Import-Module $projectName -Force -PassThru -ErrorAction 'Stop'
 }
 
-Context 'Install-GuestConfigurationPackage' {
+Describe 'Install-GuestConfigurationPackage' -ForEach @{
+    ProjectPath    = $projectPath
+    projectName    = $projectName
+    importedModule = $importedModule
+} {
     BeforeAll {
         # test Assets path
         $testAssetsPath = Join-Path -Path $PSScriptRoot -ChildPath '../assets'

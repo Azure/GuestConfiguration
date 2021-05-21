fffd34e49f1b3637ea35b1c5230691e77b4f1b6a
Michelle Yang <micy@microsoft.com>
2021-05-20 23:55:15 -0700

    Remove comment

--- source/Public/New-GuestConfigurationPolicy.ps1
+++ source/Public/New-GuestConfigurationPolicy.ps1
@@ -134,7 +134,7 @@ function New-GuestConfigurationPolicy
         }
 
         # Generate checksum hash for policy content.
-        Invoke-WebRequest -Uri $ContentUri -OutFile $tempContentPackageFilePath -SslProtocol Tls12 -Verbose
+        Invoke-WebRequest -Uri $ContentUri -OutFile $tempContentPackageFilePath -SslProtocol Tls12
         $tempContentPackageFilePath = Resolve-Path $tempContentPackageFilePath
         $contentHash = (Get-FileHash $tempContentPackageFilePath -Algorithm SHA256).Hash
         Write-Verbose "SHA256 Hash for content '$ContentUri' : $contentHash."

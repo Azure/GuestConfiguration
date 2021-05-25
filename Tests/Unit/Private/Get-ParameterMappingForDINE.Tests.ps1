18832a76e23c4aa133eada71dfe2604bbaeef67d
Michelle Yang <micy@microsoft.com>
2021-05-25 00:37:12 -0700

    Add another scope

--- Tests/Unit/Private/Get-ParameterMappingForDINE.Tests.ps1
+++ Tests/Unit/Private/Get-ParameterMappingForDINE.Tests.ps1
@@ -20,56 +20,59 @@ Describe ' Get-ParameterMappingForDINE.ps1' -ForEach @{
 
     It 'Validate output contents' -Skip:($IsLinux -or $IsMacOS) {
         inModuleScope -ModuleName GuestConfiguration {
-            $Parameter = @(
-                @{
-                    Name = 'ensure'
-                    DisplayName = 'ensure_display_name'
-                    Description = "ensure_description"
-                    ResourceType = 'MyFile'
-                    ResourceId = 'createFoobarTestFile'
-                    ResourcePropertyName = 'ensure'
-                    DefaultValue = 'Present'
-                    AllowedValues = @('Present','Absent')
-                },
-                @{
-                    Name = 'path'
-                    DisplayName = 'path_display_name'
-                    Description = "path_description"
-                    ResourceType = 'MyFile'
-                    ResourceId = 'createFoobarTestFile'
-                    ResourcePropertyName = 'path'
-                },
-                @{
-                    Name = 'content'
-                    DisplayName = 'content_display_name'
-                    Description = "content_description"
-                    ResourceType = 'MyFile'
-                    ResourceId = 'createFoobarTestFile'
-                    ResourcePropertyName = 'content'
-                    DefaultValue = 'Example content'
-                })
-            $ParameterInfo = Update-PolicyParameter -Parameter $Parameter
-            $actualOutput = ( Get-ParameterMappingForDINE -ParameterInfo $ParameterInfo ) | ConvertTo-Json
-            # $actualOutput | Should -be $correctOutput
-            Write-Output $actualOutput
-            $expectedOutput = @(
-                @{
-                    'name'= "[MyFile]createFoobarTestFile;path"
-                    'value'= "[parameters('path')]"
-                },
-                @{
-                    'name'= "[MyFile]createFoobarTestFile;ensure"
-                    'value'= "[parameters('ensure')]"
-                },
-                @{
-                    'name'= "[MyFile]createFoobarTestFile;content"
-                    'value'= "[parameters('content')]"
-                }
-            ) | ConvertTo-Json
-            write-output $expectedOutput
-            $actualOutput  | Should -Be $expectedOutput
-            # Assert-Equivalent -Actual ($actualOutput)  -Expected ($expectedOutput)
+            inModuleScope -ModuleName GuestConfigurationPolicy {
 
+                $Parameter = @(
+                    @{
+                        Name = 'ensure'
+                        DisplayName = 'ensure_display_name'
+                        Description = "ensure_description"
+                        ResourceType = 'MyFile'
+                        ResourceId = 'createFoobarTestFile'
+                        ResourcePropertyName = 'ensure'
+                        DefaultValue = 'Present'
+                        AllowedValues = @('Present','Absent')
+                    },
+                    @{
+                        Name = 'path'
+                        DisplayName = 'path_display_name'
+                        Description = "path_description"
+                        ResourceType = 'MyFile'
+                        ResourceId = 'createFoobarTestFile'
+                        ResourcePropertyName = 'path'
+                    },
+                    @{
+                        Name = 'content'
+                        DisplayName = 'content_display_name'
+                        Description = "content_description"
+                        ResourceType = 'MyFile'
+                        ResourceId = 'createFoobarTestFile'
+                        ResourcePropertyName = 'content'
+                        DefaultValue = 'Example content'
+                    })
+                $ParameterInfo = Update-PolicyParameter -Parameter $Parameter
+                $actualOutput = ( Get-ParameterMappingForDINE -ParameterInfo $ParameterInfo ) | ConvertTo-Json
+                # $actualOutput | Should -be $correctOutput
+                Write-Output $actualOutput
+                $expectedOutput = @(
+                    @{
+                        'name'= "[MyFile]createFoobarTestFile;path"
+                        'value'= "[parameters('path')]"
+                    },
+                    @{
+                        'name'= "[MyFile]createFoobarTestFile;ensure"
+                        'value'= "[parameters('ensure')]"
+                    },
+                    @{
+                        'name'= "[MyFile]createFoobarTestFile;content"
+                        'value'= "[parameters('content')]"
+                    }
+                ) | ConvertTo-Json
+                write-output $expectedOutput
+                $actualOutput  | Should -Be $expectedOutput
+                # Assert-Equivalent -Actual ($actualOutput)  -Expected ($expectedOutput)
+
+            }
         }
     }
 }

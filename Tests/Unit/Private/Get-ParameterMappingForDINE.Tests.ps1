acc8bdc4832f31f24a4c0a808465664e4eceb111
Michelle Yang <micy@microsoft.com>
2021-05-24 18:51:15 -0700

    Add test for new function

--- Tests/Unit/Private/Get-ParameterMappingForDINE.Tests.ps1
+++ Tests/Unit/Private/Get-ParameterMappingForDINE.Tests.ps1
@@ -19,76 +19,50 @@ Describe ' Get-ParameterMappingForDINE.ps1' -ForEach @{
     }
 
     It 'Validate output contents' -Skip:($IsLinux -or $IsMacOS) {
-        inModuleScope -ModuleName GuestConfiguration {
-            inModuleScope -ModuleName GuestConfigurationPolicy {
-                # Calculate actual output
-                $Parameter = @(
-                    @{
-                        Name = 'ensure'
-                        DisplayName = 'ensure_display_name'
-                        Description = "ensure_description"
-                        ResourceType = 'MyFile'
-                        ResourceId = 'createFoobarTestFile'
-                        ResourcePropertyName = 'ensure'
-                        DefaultValue = 'Present'
-                        AllowedValues = @('Present','Absent')
-                    },
-                    @{
-                        Name = 'path'
-                        DisplayName = 'path_display_name'
-                        Description = "path_description"
-                        ResourceType = 'MyFile'
-                        ResourceId = 'createFoobarTestFile'
-                        ResourcePropertyName = 'path'
-                    },
-                    @{
-                        Name = 'content'
-                        DisplayName = 'content_display_name'
-                        Description = "content_description"
-                        ResourceType = 'MyFile'
-                        ResourceId = 'createFoobarTestFile'
-                        ResourcePropertyName = 'content'
-                        DefaultValue = 'Example content'
-                    })
-                $ParameterInfo = Update-PolicyParameter -Parameter $Parameter
-                $actualOutput = ( Get-ParameterMappingForDINE -ParameterInfo $ParameterInfo )
 
-                # Expected output
-                $expectedOutput = @(
-                    @{
-                        'name'= "[MyFile]createFoobarTestFile;path"
-                        'value'= "[parameters('path')]"
-                    },
-                    @{
-                        'name'= "[MyFile]createFoobarTestFile;ensure"
-                        'value'= "[parameters('ensure')]"
-                    },
-                    @{
-                        'name'= "[MyFile]createFoobarTestFile;content"
-                        'value'= "[parameters('content')]"
-                    }
-                )
+        $ParameterInfo = @(
+            @{
+                Name = 'ensure'
+                DisplayName = 'ensure_display_name'
+                Description = "ensure_description"
+                ResourceType = 'MyFile'
+                ResourceId = 'createFoobarTestFile'
+                ResourcePropertyName = 'ensure'
+                DefaultValue = 'Present'
+                AllowedValues = @('Present','Absent')
+            },
+            @{
+                Name = 'path'
+                DisplayName = 'path_display_name'
+                Description = "path_description"
+                ResourceType = 'MyFile'
+                ResourceId = 'createFoobarTestFile'
+                ResourcePropertyName = 'path'
+            },
+            @{
+                Name = 'content'
+                DisplayName = 'content_display_name'
+                Description = "content_description"
+                ResourceType = 'MyFile'
+                ResourceId = 'createFoobarTestFile'
+                ResourcePropertyName = 'content'
+                DefaultValue = 'Example content'
+            })
 
-                # Comapare expected output to actual output
-                $actualOutputArray = @()
-                foreach ($object in $actualOutput)
-                {
-                    Write-Verbose "output $($object | ConvertTo-Json -Compress | Out-String)"
-                    $actualOutputArray += $object | ConvertTo-Json -Compress | Out-String
-                }
-
-                $expectedOutputArray = @()
-                foreach ($object in $expectedOutput)
-                {
-                    Write-Verbose "output $($object | ConvertTo-Json -Compress | Out-String)"
-                    $expectedOutputArray += $object | ConvertTo-Json -Compress | Out-String
-                }
-
-                $diff = @()
-                $diff += ($expectedOutputArray | ?{$actualOutputArray -notcontains $_})
-                $diff += ($actualOutputArray | ?{$expectedOutputArray -notcontains $_})
-                $diff.Count | Should -Be 0
+        $correctOutput = @(
+            @{
+                'name'= "[MyFile]createFoobarTestFile;path"
+                'value'= "[parameters('path')]"
+            },
+            @{
+                'name'= "[MyFile]createFoobarTestFile;ensure"
+                'value'= "[parameters('ensure')]"
+            },
+            @{
+                'name'= "[MyFile]createFoobarTestFile;content"
+                'value'= "[parameters('content')]"
             }
-        }
+        )
+        Get-ParameterMappingForDINE $ParameterInfo | Should -be $correctOutput
     }
 }

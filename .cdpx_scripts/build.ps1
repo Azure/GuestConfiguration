5c368b3d6cbf8a509932b8993186fe2d42d224ec
Michelle Yang <micy@microsoft.com>
2021-06-02 13:51:13 -0700

    Add restore step to resolve dependencies

--- .cdpx_scripts/build.ps1
+++ .cdpx_scripts/build.ps1
@@ -1,4 +1,5 @@
 Write-Output "About to run build"
+Write-Output "$PSVersionTable"
 Write-Verbose "About to run build" -Verbose
 
 $RootPath = [io.Path]::GetFullPath("$PSScriptRoot/..")
@@ -12,6 +13,7 @@ $location = Join-Path $currentDir "\.."
 $env:APPVEYOR_BUILD_FOLDER = $location
 $destination = Join-Path $env:SystemDrive 'out'
 
-../build.ps1 -Tasks noop -ResolveDependency
+# Actually run build
+# ../build.ps1 -Tasks noop -ResolveDependency
 ../build.ps1 -Tasks build
 Write-Verbose "Exiting build script..." -Verbose
---
+++ .cdpx_scripts/restore.ps1
@@ -0,0 +1,1 @@
+../build.ps1 -Tasks noop -ResolveDependency

744acf1d583d0c19b845a32fa7abeb52d10b740d
Michelle Yang <micy@microsoft.com>
2021-06-03 12:23:30 -0700

    Update output

--- .cdpx_scripts/package.ps1
+++ .cdpx_scripts/package.ps1
@@ -1,3 +1,4 @@
 # Package the files
 # TODO: Look at what the other pipeline does! The old one -- azure pipelines public
+Write-Verbose "About to run pack command" -Verbose
 ../build.ps1 -tasks pack

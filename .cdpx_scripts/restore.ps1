ac18a2b57cee27af11dae005ab342a2d15b20a5d
Michelle Yang <micy@microsoft.com>
2021-06-03 18:00:29 -0700

    Add Import module into the test

--- .cdpx_scripts/restore.ps1
+++ .cdpx_scripts/restore.ps1
@@ -1,2 +1,4 @@
 # Run Build Command
-../build.ps1 -Tasks build,test -ResolveDependency
+../build.ps1 -Tasks build -ResolveDependency
+Import-Module GuestConfiguration -Force
+../build.ps1 -Tasks test

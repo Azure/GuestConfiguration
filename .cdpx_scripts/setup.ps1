10255778bcbc30baab3ca79a1c2406b8a39d06fa
Michelle Yang <micy@microsoft.com>
2021-06-09 16:14:22 -0700

    Try to make sure that env variable is passed

--- .cdpx_scripts/setup.ps1
+++ .cdpx_scripts/setup.ps1
@@ -1,11 +1,14 @@
 # Validate PS Version
 $PSVersionTable
+Write-Verbose "$env:GitHubToken" -Verbose
+Write-Verbose "$env:GITHUBTOKEN" -Verbose
 
 # Run build command, resolve ependencies (This step needs internet)
 ../build.ps1 -Tasks build -ResolveDependency
 
-# Set up environment
+Write-Verbose "Printing out contents of bin folder" -Verbose
 Get-ChildItem "C:\source\output\GuestConfiguration\3.2.0\bin\"
+# Set up environment
 $env:PSModulePath = "C:\source\output\" + ';' + $env:PSModulePath
 Get-Module GuestConfiguration -ListAvailable
 Import-Module GuestConfiguration -Force
